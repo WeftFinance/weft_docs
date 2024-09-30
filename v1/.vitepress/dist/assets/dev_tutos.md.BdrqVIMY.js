5cbcb0593299adfac7ff1bec9e67325ccfd2511b
atoumbre <atoumbre@gmail.com>
2024-09-30 17:33:44 +0000

    clean up

--- v1/.vitepress/dist/assets/dev_tutos.md.BdrqVIMY.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as e,o as a}from"./chunks/framework.BDixHSM8.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"dev/tutos.md","filePath":"dev/tutos.md","lastUpdated":1727701684000}'),o={name:"dev/tutos.md"};function s(r,c,d,n,p,_){return a(),e("div")}const f=t(o,[["render",s]]);export{m as __pageData,f as default};
